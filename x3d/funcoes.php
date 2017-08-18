c091d76e16ac9e267b3e0beebea811fd94249642
Guilherme <Guilherme@10.1.9.59>
2017-08-18 11:00:51 -0300

    Corrigido o bug de visualização do manequim.

--- x3d/funcoes.php
+++ x3d/funcoes.php
@@ -1,15 +1,15 @@
 
 <?php
     function cenario($nick){
-        echo"<x3d width='400px' height='650px'>";
-        echo"<scene id='cena'>";
-        echo"<directionalLight id='directional' direction='0 0 1' on ='TRUE' intensity='1.0' shadowIntensity='0.0'> </directionalLight>";
-        echo"<navigationInfo id='head' headlight='true' type='\"EXAMINE\"'>  </navigationInfo>";
-        echo"<PointLight id='point' on='TRUE' intensity='0.9000' color='0.0 0.6 0.0' location='0 10 0.5 ' radius='5.0000' >  </PointLight>";
-        echo"<SpotLight id='spot' on =\"TRUE\" beamWidth='0.9' color='0 0 1' cutOffAngle='0.78' location='0 0 12' radius='22' >  </SpotLight>";
-        //echo"<viewpoint position='-1.94639 1.79771 -2.89271' orientation='0.03886 0.99185 0.1213";
-        echo"<Inline nameSpaceName=\"$nick\" mapDEFToID=\"true\" url=\"dummy/$nick.x3d\" />";
-        echo"</scene>";
+        echo" <x3d width='400px' height='650px'>";
+           echo"<scene>";
+                echo"<directionalLight id='directional' direction='0 0 1' on ='TRUE' intensity='1.0' shadowIntensity='0.0'> </directionalLight>";
+                echo" <navigationInfo id='head' headlight='true' type='\"EXAMINE\"'>  </navigationInfo> ";
+                echo"<PointLight id='point' on='TRUE' intensity='0.9000' color='0.0 0.6 0.0' location='0 10 0.5 ' radius='5.0000' >  </PointLight>"; 
+                echo"<SpotLight id='spot' on =\"TRUE\" beamWidth='0.9' color='0 0 1' cutOffAngle='0.78' location='0 0 12' radius='22' >  </SpotLight>";
+                echo"<!--position X, Y, x --> <viewpoint position=\"-1.94639 1.79771 -2.89271\" orientation=\"0.03886 0.99185 0.12133 3.75685\"></viewpoint>";
+                echo"<Inline nameSpaceName=\"$nick\" mapDEFToID=\"true\" url=\"$nick.x3d\" />";
+            echo"</scene>";
         echo"</x3d>";
     }
 
--- x3d/index.php
+++ x3d/index.php
@@ -44,18 +44,7 @@ and open the template in the editor.
         }
         $tam = 0;
     ?>
-        
    </div>
-    <!-- <x3d width='400px' height='650px'>
-        <scene>
-            <directionalLight id="directional" direction='0 0 1' on ="TRUE" intensity='1.0' shadowIntensity='0.0'> </directionalLight>
-            <navigationInfo id="head" headlight='true' type='"EXAMINE"'>  </navigationInfo> 
-            <PointLight id='point' on='TRUE' intensity='0.9000' color='0.0 0.6 0.0' location='0 10 0.5 ' radius='5.0000' >  </PointLight> 
-            <SpotLight id='spot' on ="TRUE" beamWidth='0.9' color='0 0 1' cutOffAngle='0.78' location='0 0 12' radius='22' >  </SpotLight>
-            <!--position X, Y, x -- <viewpoint position="-1.94639 1.79771 -2.89271" orientation="0.03886 0.99185 0.12133 3.75685"></viewpoint>
-            <Inline nameSpaceName="teste_roupa" mapDEFToID="true" url="teste_roupa.x3d" />
-        </scene>
-    </x3d>-->
 </body>
 </html>
 
